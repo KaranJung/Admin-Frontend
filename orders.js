22083ee1d82118cf569e3d6755eaf0376c6f6008
Karan Jung Budhathoki <97191668+KaranJung@users.noreply.github.com>
2025-01-04 16:48:33 +0545

    Add files via upload

---
+++ orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: 'Iphone 12 Pro Max',
+        productNumber: '85743',
+        paymentStatus: 'Due',
+        status: 'Pending'
+    },
+    {
+        productName: 'Iphone 11 Pro Max',
+        productNumber: '97245',
+        paymentStatus: 'Refunded',
+        status: 'Declined'
+    },
+    {
+        productName: 'Iphone 12 Pro',
+        productNumber: '36452',
+        paymentStatus: 'Paid',
+        status: 'Active'
+    },
+]
